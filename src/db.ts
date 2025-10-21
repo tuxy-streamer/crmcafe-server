86c170ef7a5c9328b42e3762e26a07df47fe377f
tuxy <subham020304@gmail.com>
2025-10-21 13:06:51 +0530

    file moved to db folder

--- src/db.ts
+++
@@ -1,6 +0,0 @@
-import postgres from "postgres";
-
-export const sql = postgres(process.env.DATABASE_URL!, {
-  prepare: true,
-  ssl: process.env.PGSSLMODE === "require" ? { rejectUnauthorized: false } : undefined,
-});
