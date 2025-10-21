a387a256d72aab7a2ce87ab60e68279470b9bfcf
Omkar-888 <omkarpadhy888@gmail.com>
2025-10-21 14:12:37 +0530

    add all routes and update the const sql

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import Fastify, { type FastifyInstance } from "fastify";
 import { loggerOptions } from "./util/log";
 import { IndexRoute } from "./routes/index";
 import { HealthRoute } from "./routes/health";
+import { registerAllRoutes } from "./routes/register";
 
 const server: FastifyInstance = Fastify({ logger: loggerOptions });
 
 server.route(IndexRoute);
 server.route(HealthRoute);
+await registerAllRoutes(server);
 
 server.listen(
   {
